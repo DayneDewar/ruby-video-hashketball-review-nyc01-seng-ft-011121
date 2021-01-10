62814ac88f7f1c6766deb51d9a9368ddec0b9f18
Dayne Dewar <daynedewar@yahoo.com>
2021-01-10 20:59:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -182,13 +182,11 @@ def player_numbers(team)
 end
 
 def player_stats(player)
-  stats = {}
   game_hash.each do |k, v|
     v[:players] do |starting_five|
       if starting_five[:player_name] == player
-        stats << starting_five
+        return starting_five
       end
     end
   end
-  stats
 end
